bb81717d768dcce6b7b0763a4ab2239b693cb49b
trungpv <trungphamvanvn@gmail.com>
2023-01-11 11:00:48 +0700

    up

---
+++ PetStore.Model/About/AboutModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using PetStore.Model.BaseEntity;
+using PetStore.Model.Files;
+
+namespace PetStore.Model.About
+{
+    public class AboutModel : BaseEntityModel
+    {
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string Image { get; set; }
+
+        public IFormFile? filesadd { get; set; }
+
+        public List<FilesModel>? FilesModels { get; set; }
+
+        public AboutModel()
+        {
+            FilesModels = new List<FilesModel>();
+        }
+    }
+}
