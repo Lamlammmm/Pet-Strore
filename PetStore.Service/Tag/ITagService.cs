a871b298242a43be03727c77344fd7a0cf94ecec
lâm vũ <botay417@gmail.com>
2023-01-13 08:34:22 +0700

    up

---
+++ PetStore.Service/Tag/ITagService.cs
@@ -0,0 +1,19 @@
+﻿using Pet_Store.Data.Entities;
+
+namespace PetStore.Service
+{
+    public interface ITagService
+    {
+        Task<IList<Tag>> GetAll();
+
+        Task<Tag> GetById(Guid id);
+
+        Task<int> Create(Tag model);
+
+        Task<int> Update(Tag modil);
+
+        Task<int> Delete(Guid id);
+
+        Task<int> DeleteByIds(IEnumerable<Guid> ids);
+    }
+}
---
+++ PetStore.Service/Tag/TagService.cs
@@ -0,0 +1,57 @@
+﻿using Pet_Store.Data.EF;
+using Pet_Store.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetStore.Service
+{
+    public class TagService:ITagService
+    {
+        private readonly PetStoreDbContext _dbContext;
+        public TagService(PetStoreDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<int> Create(Tag model)
+        {
+            var item = new Tag()
+            {
+                Id = Guid.NewGuid(),
+                BlogId=model.BlogId,
+                NameTag=model.NameTag,
+            };
+            await _dbContext.Tags.AddAsync(item);
+            var result = await _dbContext.SaveChangesAsync();
+            return result;
+        }
+
+        public Task<int> Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeleteByIds(IEnumerable<Guid> ids)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<Tag>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Tag> GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Update(Tag modil)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
